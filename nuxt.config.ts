fd99165c7471dcb52b644b2d96c4bbb02bdc581a
ppppppsmash <pppppp.work@gmail.com>
2023-10-19 21:07:07 +0900

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@unocss/nuxt',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/eslint-module',
+    '@vueuse/nuxt',
+  ],
+  googleFonts: {
+    prefetch: true,
+    preconnect: true,
+    preload: true,
+    download: true,
+    families: {
+      'Inter': true,
+      'Zen Maru Gothic': true,
+    },
+  },
+  eslint: {
+    lintOnStart: false,
+  },
 })
