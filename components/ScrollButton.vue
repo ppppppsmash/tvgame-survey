473fa03133b8e6ad582add3d41a71c804ef8c44c
ppppppsmash <pppppp.work@gmail.com>
2023-10-21 21:53:33 +0900

    feat: slide down animation

--- components/ScrollButton.vue
+++ components/ScrollButton.vue
@@ -1,37 +1,38 @@
-<script setup>
-
+<script setup lang="ts">
 </script>
 
 <template>
-  <div class="absolute bottom-12 left-1/2 -translate-x-1/2">
+  <div class="absolute bottom-12 left-1/2 -translate-x-1/2
+    hover:-translate-y-1 hover:scale-95 duration-300">
     <NuxtLink
       to="/survey"
+      class="block"
     >
       <div class="relative w-ful flex justify-center h-[24px]">
         <div
           class="z-50 absolute w-[40px] h-[8px] opacity-0 before:absolute before:content-[' ']
-          before:top-4 before:h-full before:w-[51%] before:bg-secondary/30 after:content-[' ']
-          after:absolute after:top-4 after:h-full after:w-[51%] after:bg-secondary/30
-          before:left-0 after:right-0 after:w-1/2 before:skew-y-30
-          after:-skew-y-30
-          animate-scroll-move-default
-          animate-scroll-move-primary"
+            before:top-4 before:h-full before:w-[51%] before:bg-secondary/30 after:content-[' ']
+            after:absolute after:top-4 after:h-full after:w-[51%] after:bg-secondary/30
+            before:left-0 after:right-0 after:w-1/2 before:skew-y-30
+            after:-skew-y-30
+            animate-scroll-move-default
+            animate-scroll-move-primary"
         />
         <div class="z-50 absolute w-[40px] h-[8px] opacity-0 before:absolute before:content-[' ']
-          before:top-4 before:h-full before:w-[51%] before:bg-secondary/30 after:content-[' ']
-          after:absolute after:top-4 after:h-full after:w-[51%] after:bg-secondary/30
-          before:left-0 after:right-0 after:w-1/2 before:skew-y-30
-          after:-skew-y-30
-          animate-scroll-move-default
-          animate-scroll-move-secondary"
+            before:top-4 before:h-full before:w-[51%] before:bg-secondary/30 after:content-[' ']
+            after:absolute after:top-4 after:h-full after:w-[51%] after:bg-secondary/30
+            before:left-0 after:right-0 after:w-1/2 before:skew-y-30
+            after:-skew-y-30
+            animate-scroll-move-default
+            animate-scroll-move-secondary"
         
         />
         <div class="z-50 absolute w-[40px] h-[8px] opacity-0 before:absolute before:content-[' ']
-          before:top-4 before:h-full before:w-[51%] before:bg-secondary/30 after:content-[' ']
-          after:absolute after:top-4 after:h-full after:w-[51%] after:bg-secondary/30
-          before:left-0 after:right-0 after:w-1/2 before:skew-y-30
-          after:-skew-y-30
-          animate-scroll-move-default"
+            before:top-4 before:h-full before:w-[51%] before:bg-secondary/30 after:content-[' ']
+            after:absolute after:top-4 after:h-full after:w-[51%] after:bg-secondary/30
+            before:left-0 after:right-0 after:w-1/2 before:skew-y-30
+            after:-skew-y-30
+            animate-scroll-move-default"
         />
         <span class="animate-pulse text-primary text-xs font-bold">
           SCROLL DOWN
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const isClick = ref(false)
+
+const animationHandler = () => {
+  isClick.value = true
+}
 </script>
 
 <template>
-  <div class="w-[100vw] h-[100vh] relative bg-neutral-2">
+  <div
+    class="w-[100vw] h-[100vh] relative bg-neutral-2 animate-slide-in-bottom"
+    :class="isClick ? 'animate-slide-in-bottom' : ''"
+  >
     <ThreeModelScene />
 
     <h1 class="absolute right-8 bottom-8 text-neutral-7">
