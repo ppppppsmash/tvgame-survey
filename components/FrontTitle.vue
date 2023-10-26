0b4391dc948baedbd6d2cebf1ebe0b97ae995e6c
ppppppsmash <pppppp.work@gmail.com>
2023-10-27 00:07:08 +0900

    style: LogoTitle

---
+++ components/FrontTitle.vue
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<template>
+  <div class="bg-primary flex items-center justify-center w-full h-screen">
+    <div class="relative border-[12px] border-[#FFFFFF] rounded-full px-10 py-2">
+      <h1 class="text-8xl text-white font-semibold">WebCrew</h1>
+      <p class="absolute top-4 right-6 text-white text-xl font-semibold">&reg;</p>
+    </div>
+  </div>
+</template>
