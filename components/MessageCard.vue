468f91bb243e9db1adcfcb87e31c953039da6d0b
ppppppsmash <pppppp.work@gmail.com>
2023-10-29 22:32:47 +0900

    chore: install laser-pen

--- components/MessageCard.vue
+++ components/MessageCard.vue
@@ -12,12 +12,12 @@ const handleNext = () => {
   <div v-if="isMessageShow">
     <div class="sm:py-6 mt-6 mb-10">
       <div>
-        <p class="font-default font-black mb-6 text-h6 text-primary">
+        <p class="font-default font-black mb-6 text-h6 text-white">
           この度はゲーム会にご参加いただきありがとうございました！<br>
           楽しんでいただけましたでしょうか。
         </p>
 
-        <p class="text-h6 text-primary font-black">
+        <p class="text-h6 text-white font-black">
           皆さんからのフィードバックをもとに、今後のイベント開催の参考にさせていただきたいと思っております。<br>
           ご意見・ご感想をお聞かせくださいますようお願いします。
         </p>
