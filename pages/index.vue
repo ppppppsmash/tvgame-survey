5751b893b8705b81c5e245cc1764d8c66784e10a
ppppppsmash <pppppp.work@gmail.com>
2023-11-05 23:13:16 +0900

    feat: ready fot the release

--- pages/index.vue
+++ pages/index.vue
@@ -8,12 +8,12 @@ const animationHandler = () => {
 
 <template>
   <div
-    class="w-[100vw] h-[100dvh] overflow-hidden relative animate-slide-in-bottom"
+    class="w-full h-[100dvh] overflow-hidden relative animate-slide-in-bottom"
     :class="isClick ? 'animate-slide-in-bottom' : ''"
   >
     <ThreeModelScene />
 
-    <HeroTitle addClass="absolute left-1/2 -translate-x-1/2 sm:left-auto sm:translate-x-0 sm:right-8 bottom-6 sm:bottom-16 h-[180px]" />
+    <HeroTitle addClass="absolute left-1/2 -translate-x-[calc(50%+1rem)] sm:left-auto sm:-translate-x-0 sm:right-6 bottom-4 sm:bottom-8 h-[180px]" />
 
     <ScrollButton />
   </div>
