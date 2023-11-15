187c6a73bb7c5a7b75edb2f0969840832476e3c9
ppppppsmash <pppppp.work@gmail.com>
2023-11-15 22:12:07 +0900

    fix: further compress the image

--- components/ThreeModelScene.vue
+++ components/ThreeModelScene.vue
@@ -86,7 +86,6 @@ onMounted(async () => {
   const imgPathList = [
     '/images/photo1.jpg',
     '/images/photo2.jpg',
-    '/images/photo3.jpg',
     '/images/photo4.jpg',
     '/images/photo5.jpg',
     '/images/photo6.jpg',
