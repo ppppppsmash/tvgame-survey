2fb5dc06436c5d00684d38a30022bd33684b7839
ppppppsmash <pppppp.work@gmail.com>
2024-10-03 19:52:42 +0900

    feat: Update image files and adjust ThreeModelScene component

--- components/ThreeModelScene.vue
+++ components/ThreeModelScene.vue
@@ -84,18 +84,17 @@ onMounted(async () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
   threeSceneRef.value.appendChild(renderer.domElement)
   const imgPathList = [
-    '/images/photo1.png',
-    '/images/photo2.png',
-    '/images/photo3.png',
-    '/images/photo4.png',
-    '/images/photo5.png',
-    '/images/photo6.png',
-    '/images/photo7.png',
-    '/images/photo8.png',
-    '/images/photo9.png',
-    '/images/photo10.png',
-    '/images/photo11.png',
-    '/images/photo12.png'
+    '/images/pic1.jpg',
+    '/images/pic2.jpg',
+    '/images/pic3.jpg',
+    '/images/pic4.jpg',
+    '/images/pic5.jpg',
+    '/images/pic6.jpg',
+    '/images/pic7.jpg',
+    '/images/pic8.jpg',
+    '/images/pic9.jpg',
+    '/images/pic10.jpg',
+    '/images/pic11.jpg',
   ]
 
   const n = 20
